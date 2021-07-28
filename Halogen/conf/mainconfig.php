f7f294e5d3fc3e09190d528cea862eebe66a9546
M41den <38406005+cradio@users.noreply.github.com>
2021-07-28 20:19:06 +0300

    Bump Logger, DBM verbose logging

--- Halogen/conf/mainconfig.php
+++ Halogen/conf/mainconfig.php
@@ -1,5 +1,6 @@
 <?php
 
 define("LOG_FILE", "../log.txt");
+define("LOG_DB_REQUESTS", false);
 define("SESS_EVERYTIME_CHECK", 0); //1 ➡ Check every request, 2 ➡ Check hourly
 define("SOUNDCLOUD_API_KEY",null);
