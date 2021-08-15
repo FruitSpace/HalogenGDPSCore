e16a9d851c80f5e1cd3d9b8ea4f50847cfbeb997
M41den <38406005+cradio@users.noreply.github.com>
2021-08-15 20:05:04 +0300

    getScores Done

--- api/score/getScores.php
+++ api/score/getScores.php
@@ -14,9 +14,51 @@ if(LOG_ENDPOINT_ACCESS){
 	$former="$ip accessed endpoint ".__FILE__;
 	err_handle("ENDPOINT","verbose",$former);
 }
+$type=(empty($_POST['type'])?"top":$_POST['type']);
 $dbm=new DBManagement();
 $acc=new CAccount($dbm);
-$users=$acc->getLeaderboard(CLEADERBOARD_BY_CPOINTS);
+switch($type){
+	case "relative":
+		$uid=(int)$_POST['accountID'];
+		$gjp=exploitPatch_remove($_POST['gjp']);
+		if($lsec->verifySession($dbm, $uid, $ip, $gjp)) {
+			$acc->uid=$uid;
+			$acc->loadStats();
+			$users = $acc->getLeaderboard(CLEADERBOARD_GLOBAL,null,$acc->stars);
+		}else{
+			$users=array();
+		}
+		break;
+	case "friends":
+		$uid=(int)$_POST['accountID'];
+		$gjp=exploitPatch_remove($_POST['gjp']);
+		if($lsec->verifySession($dbm, $uid, $ip, $gjp)) {
+			$acc->uid=$uid;
+			$acc->loadSocial();
+			require_once __DIR__ . "/../../halcore/CAccount.php";
+			require_once __DIR__ . "/../../halcore/CFriendship.php";
+			$cf=new CFriendship($dbm);
+			if($acc->friendsCount==0){$users=array();break;}
+			$friendships=explode(",",$acc->friendshipIds);
+			$friend_ids=array();
+			foreach ($friendships as $frid){
+				$ids=$cf->getFriendByFID($frid);
+				$fid=($ids['uid1']==$uid?$ids['uid2']:$ids['uid1']);
+				array_push($friendships,$fid);
+			}
+			$users = $acc->getLeaderboard(CLEADERBOARD_GLOBAL,$friend_ids);
+		}else{
+			$users=array();
+		}
+		break;
+	case "creators":
+		$users=$acc->getLeaderboard(CLEADERBOARD_BY_CPOINTS);
+		break;
+	case "top":
+	default:
+		$users=$acc->getLeaderboard(CLEADERBOARD_BY_STARS);
+}
+
 if(empty($users)) die("");
 $output="";
 $lk=0;
@@ -32,4 +74,3 @@ foreach ($users as $usr){
 	$output.=":15:".$cacc->special.":16:".$cacc->uid.":17:".$cacc->ucoins.":46:".$cacc->diamonds."|";
 }
 echo substr($output,0,-1);
-//:17::15::16::46:|";
