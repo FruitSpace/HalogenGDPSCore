fa9f393174d289b38aaa4cfbc45794b39738bab7
M41den <38406005+cradio@users.noreply.github.com>
2021-08-17 20:28:56 +0300

    Fix F3

--- halcore/CLevelFilter.php
+++ halcore/CLevelFilter.php
@@ -79,6 +79,7 @@ class CLevelFilter{
 				$whereq.=" AND track_id=".$params['songid'];
 			}
 		} //Song NG/Classic stuff
+		err_handle("TMP","verbose",json_encode($whereq));
 		return $whereq;
 	}
 
