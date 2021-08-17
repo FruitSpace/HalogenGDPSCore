2366c6f2415063cc03831dbef9101a7b8b4d7479
M41den <38406005+cradio@users.noreply.github.com>
2021-08-17 23:09:12 +0300

    Fix idk whicj

--- api/level/level_getLevels.php
+++ api/level/level_getLevels.php
@@ -191,6 +191,6 @@ foreach($levels as $slevel){
 	$output.=":19:".$cl->isFeatured.":25:".$auto.":30:".$cl->origId.":31:".$cl->is2p.":35:".$cl->song_id.":37:".$cl->ucoins.":38:".$cl->coins;
 	$output.=":39:".$cl->starsRequested.":42:".$cl->isEpic.":43:".($cl->demonDifficulty>=0?$cl->demonDifficulty:"3").":45:".$cl->objects.":46:1:47:2|";
 
-	$hashstr.=$cl->id[0].$cl->id[strlen($cl->id)-1].$cl->starsGot.$cl->coins;
+	$hashstr.=((string)$cl->id)[0].((string)$cl->id)[strlen(((string)$cl->id))-1].$cl->starsGot.$cl->coins;
 }
 echo substr($output,0,-1)."#".substr($userstring,0,-1)."##".count($levels).":".$page.":10#".genhash_genSolo2($hashstr);
