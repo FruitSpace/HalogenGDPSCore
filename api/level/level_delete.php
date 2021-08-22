eec7f453325c3435da305c2ebc3b16d758723f68
M41den <38406005+cradio@users.noreply.github.com>
2021-08-23 00:08:15 +0300

    Action Logging Everywhere

--- api/level/level_delete.php
+++ api/level/level_delete.php
@@ -22,7 +22,11 @@ if(isset($_POST['accountID']) and isset($_POST['levelID']) and isset($_POST['gjp
 		if($cl->isOwnedBy($uid)){
 			$cl->deleteLevel();
 			require_once __DIR__."/../../halcore/lib/actions.php";
-			registerAction(ACTION_LEVEL_DELETE,$uid,$id,array(),$dbm);
+			require_once __DIR__."/../../halcore/CAccount.php";
+			$acc=new CAccount($dbm);
+			$acc->uid=$uid;
+			$acc->loadAuth();
+			registerAction(ACTION_LEVEL_DELETE,$uid,$id,array("uname"=>$acc->uname,"type"=>"Delete (Owner)"),$dbm);
 			echo "1";
 		}else{
 			echo "-1";
--- halcore/lib/modCommandProcessor.php
+++ halcore/lib/modCommandProcessor.php
@@ -1,30 +1,39 @@
 <?php
 
-function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=false, $privs=null){
+function invokeCommands(DBManagement $dbm,int $lvl_id, CAccount $acc, $comment, bool $isOwner=false, $privs=null){
 	$command=explode(" ",$comment);
+	require_once __DIR__."/../../halcore/lib/actions.php";
+	require_once __DIR__."/../../halcore/CAccount.php";
+	$acc->loadAuth();
 	switch($command[0]){
 		case "!feature":
 			if($isOwner or $privs['cFeature']!=1) return -1;
+			registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Feature (Mod)"),$dbm);
 			$dbm->query("UPDATE levels SET isFeatured=1 WHERE id=$lvl_id");
 			return 1;
 		case "!unfeature":
 			if($isOwner or $privs['cFeature']!=1) return -1;
+			registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Unfeature (Mod)"),$dbm);
 			$dbm->query("UPDATE levels SET isFeatured=0 WHERE id=$lvl_id");
 			return 1;
 		case "!epic":
+			registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Epic (Mod)"),$dbm);
 			if($isOwner or $privs['cEpic']!=1) return -1;
 			$dbm->query("UPDATE levels SET isEpic=1 WHERE id=$lvl_id");
 			return 1;
 		case "!unepic":
 			if($isOwner or $privs['cEpic']!=1) return -1;
+			registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Unepic (Mod)"),$dbm);
 			$dbm->query("UPDATE levels SET isEpic=0 WHERE id=$lvl_id");
 			return 1;
 		case "!coins":
 			if($isOwner or $privs['cVerCoins']!=1) return -1;
 			if(!isset($command[1])) return -1;
 			if($command[1]=="verify"){
+				registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Coins:Verify (Mod)"),$dbm);
 				$dbm->query("UPDATE levels SET coins=ucoins WHERE id=$lvl_id");
 			}elseif($command[1]=="reset"){
+				registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Coins:Reset (Mod)"),$dbm);
 				$dbm->query("UPDATE levels SET coins=0 WHERE id=$lvl_id");
 			}else{
 				return -1;
@@ -33,16 +42,20 @@ function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=fa
 		case "!daily":
 			if($isOwner or $privs['cDaily']!=1) return -1;
 			if(isset($command[1]) and $command[1]=="reset"){
+				registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Daily:Reswt (Mod)"),$dbm);
 				$dbm->query("DELETE FROM quests WHERE lvl_id=$lvl_id");
 			}else{
+				registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Daily:Publish (Mod)"),$dbm);
 				$dbm->query("INSERT INTO quests (type,lvl_id,timeExpire) VALUES(0,$lvl_id,'". date("Y-m-d H:i:s",strtotime("tomorrow"))."')");
 			}
 			return 1;
 		case "!weekly":
 			if($isOwner or $privs['cWeekly']!=1) return -1;
 			if(isset($command[1]) and $command[1]=="reset"){
+				registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Weekly:Reset (Mod)"),$dbm);
 				$dbm->query("DELETE FROM quests WHERE lvl_id=$lvl_id");
 			}else{
+				registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Weekly:Publish (Mod)"),$dbm);
 				$dbm->query("INSERT INTO quests (type,lvl_id,timeExpire) VALUES(1,$lvl_id,'".date("Y-m-d H:i:s",strtotime("next monday"))."')");
 			}
 			return 1;
@@ -74,6 +87,7 @@ function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=fa
 				default:
 					return -1;
 			}
+			registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Rate:".ucfirst(strtolower($command.[1]))." (Mod)"),$dbm);
 			$dbm->query("UPDATE levels SET difficulty=$diff WHERE id=$lvl_id");
 			return 1;
 
@@ -84,11 +98,13 @@ function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=fa
 				case "delete":
 					if($isOwner or $privs['cDelete']!=1) return -1;
 					if(!isset($command[2]) or $command[2]!=$lvl_id) return -1;
+					registerAction(ACTION_LEVEL_DELETE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Delete (".($isOwner?"Owner":"Mod").")"),$dbm);
 					$dbm->query("DELETE FROM levels WHERE id=$lvl_id");
 					return 1;
 				case "rename":
 					if(!$isOwner and $privs['cLvlAccess']!=1) return -1;
 					if(!isset($command[2])) return -1;
+					registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Rename (".($isOwner?"Owner":"Mod").")"),$dbm);
 					if(strlen(str_replace("!lvl rename ","",$comment))>32) return -1;
 					$dbm->preparedQuery("UPDATE levels SET name=? WHERE id=$lvl_id","s",str_replace("!lvl rename ","",$comment));
 					return 1;
@@ -97,13 +113,16 @@ function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=fa
 					if(!isset($command[2])) return -1;
 					switch ($command[2]){
 						case "on":
+							registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Copy:Enable (Owner)"),$dbm);
 							$dbm->query("UPDATE levels SET password=1 WHERE id=$lvl_id");
 							return 1;
 						case "off":
+							registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Copy:Disable (Owner)"),$dbm);
 							$dbm->query("UPDATE levels SET password=0 WHERE id=$lvl_id");
 							return 1;
 						case "pass":
 							if(!isset($command[3]) or !is_numeric($command[3]) or $command[3]<0 or strlen($command[3])!=6) return -1;
+							registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Copy:Password (Owner)"),$dbm);
 							$dbm->preparedQuery("UPDATE levels SET password=? WHERE id=$lvl_id","i", "1".$command[3]);
 							return 1;
 						default:
@@ -116,19 +135,23 @@ function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=fa
 					$acc=new CAccount($dbm);
 					$uid=$acc->getUIDbyUname($command[3]);
 					if($uid<1) return -1;
+					registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Chown->".ucfirst(strtolower($command[3]))." (".($isOwner?"Owner":"Mod").")"),$dbm);
 					$dbm->query("UPDATE levels SET uid=$uid WHERE id=$lvl_id");
 					return 1;
 				case "desc":
 					if(!$isOwner and $privs['cLvlAccess']!=1) return -1;
 					if(!isset($command[2]) or strlen(str_replace("!lvl desc ","",$comment))>256) return -1;
+					registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"UpdDescription (".($isOwner?"Owner":"Mod").")"),$dbm);
 					$dbm->preparedQuery("UPDATE levels SET description=? WHERE id=$lvl_id","s",base64_encode(str_replace("!lvl desc ","",$comment)));
 					return 1;
 				case "list":
 					if(!$isOwner and $privs['cLvlAccess']!=1) return -1;
+					registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"List (".($isOwner?"Owner":"Mod").")"),$dbm);
 					$dbm->query("UPDATE levels SET isUnlisted=0 WHERE id=$lvl_id");
 					return 1;
 				case "unlist":
 					if(!$isOwner and $privs['cLvlAccess']!=1) return -1;
+					registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"Unlist (".($isOwner?"Owner":"Mod").")"),$dbm);
 					$dbm->query("UPDATE levels SET isUnlisted=1 WHERE id=$lvl_id");
 					return 1;
 				case "ldm":
@@ -157,6 +180,7 @@ function invokeCommands(DBManagement $dbm,int $lvl_id,$comment, bool $isOwner=fa
 				if($command[1]>25 or $command[1]<0) return -1;
 				$dbm->preparedQuery("UPDATE levels SET song_id=0,track_id=? WHERE id=$lvl_id","i",$command[1]);
 			}
+			registerAction(ACTION_LEVEL_UPDATE,$acc->uid,$lvl_id,array("uname"=>$acc->uname,"type"=>"SongUpdate (Owner)"),$dbm);
 			return 1;
 	}
 }
