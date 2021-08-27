45a66732c94fcbb7a74c98e82917997a282783ae
M41den <38406005+cradio@users.noreply.github.com>
2021-08-27 17:22:32 +0300

    Challenge: please let this be a normal field trip

--- halcore/CQuests.php
+++ halcore/CQuests.php
@@ -2,9 +2,7 @@
 
 define("QUEST_TYPE_DAILY",200);
 define("QUEST_TYPE_WEEKLY",201);
-define("QUEST_TYPE_ORBS",202);
-define("QUEST_TYPE_COINS",203);
-define("QUEST_TYPE_STARS",204);
+define("QUEST_TYPE_CHALLENGE",202);
 
 class CQuests{
 	public $db;
@@ -14,17 +12,18 @@ class CQuests{
 	}
 
 	function exists(int $type=QUEST_TYPE_DAILY){
-		$type=$type-200;
-		return $this->db->query("SELECT count(*) as cnt FROM quests WHERE type=$type")->fetch_assoc()['cnt']>0;
+		$type="=".$type-200;
+		if($type=2) $type=">1";
+		return $this->db->query("SELECT count(*) as cnt FROM quests WHERE type$type")->fetch_assoc()['cnt']>0;
 	}
 
 	function getDaily(){
-		$req = $this->db->preparedQuery("SELECT id, lvl_id FROM quests WHERE type=0 AND timeExpire<? ORDER BY timeExpire DESC LIMIT 1","s",date("Y-m-d H:i:s"));
+		$req = $this->db->query("SELECT id, lvl_id FROM quests WHERE type=0 AND timeExpire<now() ORDER BY timeExpire DESC LIMIT 1");
 		return $req->fetch_assoc();
 	}
 
 	function getWeekly(){
-		$req = $this->db->preparedQuery("SELECT id, lvl_id FROM quests WHERE type=1 AND timeExpire<? ORDER BY timeExpire DESC LIMIT 1","s",date("Y-m-d H:i:s"));
+		$req = $this->db->query("SELECT id, lvl_id FROM quests WHERE type=1 AND timeExpire<now() ORDER BY timeExpire DESC LIMIT 1");
 		return $req->fetch_assoc();
 	}
 
@@ -43,6 +42,20 @@ class CQuests{
 		if(strlen($name)>64) return -1;
 		$this->db->preparedQuery("INSERT INTO quests (type,needed,reward,name,timeExpire) VALUES (?,?,?,?,?)","iiiss",$type,$needed,$reward,$name,date("Y-m-d H:i:s"));
 		return $this->db->getDB()->insert_id;
+	}
+
+	function getQuests(){
+		$req=$this->db->query("SELECT id,type,needed,reward,name,timeExpire FROM quests AS r1
+    		JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM quests)) AS id) AS r2
+ 			WHERE r1.id >= r2.id AND r1.timeExpire<now() AND r1.type>1 ORDER BY r1.id ASC LIMIT 3");
+		$reqm=array();
+		while($res=$req->fetch_assoc()) $reqm[]=$res;
+		while(count($reqm)<3) array_push($reqm, $reqm[0]);
+		$quests="";
+		foreach($reqm as $sreq) {
+			$quests.=$sreq['id'].",".((int)$sreq['type']-1).",".$sreq['needed'].",".$sreq['reward'].",".$sreq['name'].":";
+		}
+		return substr($quests,0,-1);
 	}
 
 	function getDailyLevel(bool $weekly){
