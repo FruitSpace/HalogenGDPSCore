a3420e99ba48b6aa49f3c9cc406d0278834e45fe
M41den <38406005+cradio@users.noreply.github.com>
2021-10-05 22:41:14 +0300

    buuuump

--- halcore/CComment.php
+++ halcore/CComment.php
@@ -95,7 +95,7 @@ class CComment{
 	}
 
 	function getAllCommentsHistory(int $uid, int $page, bool $sort_mode=false){
-		$req=$this->db->preparedQuery("SELECT id,comment,postedTime,likes,isSpam,percent FROM comments WHERE uid=? ORDER BY ".($sort_mode?"likes":"postedTime")." LIMIT 10 DESC OFFSET $page","i",$uid);
+		$req=$this->db->preparedQuery("SELECT id,comment,postedTime,likes,isSpam,percent FROM comments WHERE uid=? ORDER BY ".($sort_mode?"likes":"postedTime")." DESC LIMIT 10 OFFSET $page","i",$uid);
 		if($this->db->isEmpty($req)) return array();
 		$reqm=array();
 		while($res=$req->fetch_assoc()) $reqm[]=$res;
