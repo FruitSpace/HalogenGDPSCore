7d53e36ce4ce8cd3b4903e5c61fb9d8c0abfb4c7
M41den <38406005+cradio@users.noreply.github.com>
2022-01-06 23:24:05 +0300

    ADD Music Support

--- halcore/CMusic.php
+++ halcore/CMusic.php
@@ -1,8 +1,23 @@
 <?php
+/*
+ * Support hal: links
+ * hal:ng:int_ID (ex hal:ng:1008153 | Avenza - Wicked VIP)
+ * hal:yt:str_ID (ex hal:yt:kXLu_x0SRm4 | Earl Sweetshirt - Dont care)
+ * hal:sc:user/track (ex hal:sc:user-372594954/rukkus | IDK - Rukkus)
+ * hal:vk:ID_ID (ex hal:vk:642495334_456239057 | Aurellio Voltaire - The Night)
+ */
+
+/*
+ * Filters:
+ * NG - (Num) - not /[^0-9]/
+ * SC - (AlphaNum and -_) SLASH (AlphaNum and -_) - /([a-z\d\-\_])+[\\\/]([a-z\d\-\_])+$/i
+ * YT - (AlphaNum and -_) - /^([a-z\d\-\_])+$/i
+ * VK - (Num) UNDERSCORE (Num/0 - /^(\d)+\_(\d)+$/
+ */
 
 class CMusic{
     public $db;
-    public $id, $name, $artist, $size, $url, $isBanned;
+    public $id, $name, $artist, $size, $url;
 
     function __construct($dbm){
         $this->db=$dbm;
@@ -25,9 +40,34 @@ class CMusic{
             $this->size=$song['size'];
             $this->url=$song['url'];
             return 1;
-        }else{
-            return -1;
+        }else return -1;
+    }
+
+    function transformHalResource(){
+        $arn=explode(":",$this->url);
+        if(count($arn)!=3) return -1;
+        switch($arn[1]){
+            case "ng":
+                if(preg_match("/[^0-9]/",$arn[2])) return -1;
+                break;
+            case "sc":
+                if(!preg_match("/([a-z\d\-\_])+[\\\\\/]([a-z\d\-\_])+$/i",$arn[2])) return -1;
+                break;
+            case "yt":
+                if(!preg_match("/^([a-z\d\-\_])+$/i")) return -1;
+                break;
+            case "vk":
+                if(!preg_match("/^(\d)+\_(\d)+$/")) return -1;
+                break;
+            default:
+                return -1;
         }
+        $song=file_get_contents(HALHOST_TRIGGER_URL."?id=".SRV_ID."&key=".SRV_KEY."&action=requestSongARN&type=".$arn[1]."&id=".$arn[2]);
+        $song=json_decode($song,true);
+        if($song['status']=="ok"){
+            $this->url=$song['url'];
+        }else return -1;
+        return 1;
     }
 
     function getSong(int $id){
@@ -40,6 +80,9 @@ class CMusic{
         $this->artist=$req['artist'];
         $this->size=$req['size'];
         $this->url=$req['url'];
+        if(substr($this->url,0,4)=="hal:"){
+            if($this->transformHalResource()<0) return -1;
+        }
         return 1;
     }
 
