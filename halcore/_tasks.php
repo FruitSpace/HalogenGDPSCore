70deb8adbc669b8c84ce0dedc0c96ff525e20291
M41den <38406005+cradio@users.noreply.github.com>
2022-02-18 22:03:53 +0300

    typo

--- halcore/_tasks.php
+++ halcore/_tasks.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__."/lib/DBManagement.php";
 require_once __DIR__."/CProtect.php";
-require_once __DIR__."CMusic.php";
+require_once __DIR__."/CMusic.php";
 $dbm=new DBManagement(true);
 $protect=new CProtect($dbm);
 $music=new CMusic($dbm);
