72ddf45cf1764570a97be86d0306e1e870d9d908
M41den <38406005+cradio@users.noreply.github.com>
2022-02-18 22:01:18 +0300

    Music Download count

--- halcore/_tasks.php
+++ halcore/_tasks.php
@@ -1,9 +1,12 @@
 <?php
 require_once __DIR__."/lib/DBManagement.php";
 require_once __DIR__."/CProtect.php";
+require_once __DIR__."CMusic.php";
 $dbm=new DBManagement(true);
 $protect=new CProtect($dbm);
+$music=new CMusic($dbm);
 
 //recalculate
 $protect->resetUserLimits();
 $protect->fillLevelModel();
+$music->countDownloads();
